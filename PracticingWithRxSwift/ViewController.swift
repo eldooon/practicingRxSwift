f30bb94d20c661dc33d587657b9a4615cec48474
eldooon <eldonchan91@gmail.com>
2018-05-17 18:16:31 -0400

    First Commit

---
+++ PracticingWithRxSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PracticingWithRxSwift
+//
+//  Created by Eldon Chan on 5/17/18.
+//  Copyright © 2018 ByEldon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
