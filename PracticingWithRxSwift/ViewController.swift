17aa70d8c4f317f74ead24a4bcc95e14b8afa2a3
eldooon <eldonchan91@gmail.com>
2018-05-18 11:28:22 -0400

    Added RxSwift, RxCocoa, labels, button

--- PracticingWithRxSwift/ViewController.swift
+++ PracticingWithRxSwift/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userNameTextfield: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var logInButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
