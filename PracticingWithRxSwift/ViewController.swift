b1f30cc17b46cc3d60b813abcffc537dffc52bf3
eldooon <eldonchan91@gmail.com>
2018-05-18 15:02:50 -0400

    Completed valid/invalid button, with RxSwift

--- PracticingWithRxSwift/ViewController.swift
+++ PracticingWithRxSwift/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
 
@@ -14,15 +16,30 @@ class ViewController: UIViewController {
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var logInButton: UIButton!
     
+    var isValid : Observable<Bool> {
+        return Observable.combineLatest(userNameTextfield.rx.text.asObservable(),   passwordTextField.rx.text.asObservable(), resultSelector: { (usernameText, passwordText) in
+            (usernameText?.count)! >= 5 && (passwordText?.count)! >= 5
+        })
+    }
+    let disposebag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        isValid.subscribe(onNext:{ [unowned self] isValid in
+            self.logInButton.setTitleColor(isValid ? .green : .red, for: .normal)
+            self.logInButton.setTitle(isValid ? "Valid": "Invalid", for: .normal)
+            print("Fields are: ", isValid)
+        }).disposed(by: disposebag)
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
 
 
 }
